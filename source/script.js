02f232458ed9189ed6c6eece10a4eaf8ca009b07
kadochnikovk <kadochnikov_k@mail.ru>
2025-05-10 17:41:03 +0500

    added services generator

--- source/script.js
+++ source/script.js
@@ -1,7 +1,272 @@
 const cards = document.querySelector('#cards')
 
+const services = document.querySelector('#services')
 
-const data = [
+const cardsData = [
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/laptop.png",
+       headlineText: "Laptops",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/gaming pc.png",
+       headlineText: "PC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the content.",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/vr.png",
+       headlineText: "VR",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
+    {
+       src:  "images/acc.png",
+       headlineText: "ACC",
+       textContent: "Some quick example text to build on the card title and make up the bulk of the card's content",
+       btnText: "Go Sopping"
+    },
     {
        src:  "images/laptop.png",
        headlineText: "Laptops",
@@ -28,10 +293,14 @@ const data = [
     },
 ]
 
+const servicesData = []
+
 const createCard = (src, headlineText, textContent, btnText) => {
+    const grid = document.createElement('div')
+    grid.classList.add('col-lg-4', 'col-md-6', 'mt-4')
 
     const card = document.createElement('div')
-    card.classList.add('col-lg-4', 'col-md-6', 'mt-4', 'card')
+    card.classList.add('card')
 
     const img = document.createElement('img')
     img.src = src
@@ -54,13 +323,22 @@ const createCard = (src, headlineText, textContent, btnText) => {
 
     cardBody.append(headline, paragraph, button)
     card.append(img, cardBody)
-    console.log('карта ', card)
-    return card
+    grid.append(card)
+
+    return grid
 }
 
+const createService = () => {}
+
 alert('Начинаем генерить карточки')
-data.forEach(element => {
+cardsData.forEach(element => {
     const { src, headlineText, textContent, btnText } = element
     const card = createCard(src, headlineText, textContent, btnText)
     cards.append(card)
+})
+
+servicesData.forEach(element => {
+
+    const service = createService()
+    services.append(service)
 })
